dadf88b3d17715f7732392f6ebe475a550ce37b2
Patrick Cullen <patrick47cullen@gmail.com>
2018-03-28 18:19:15 +0100

    finished tasks

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -147,18 +147,22 @@ p evens
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 
-
+users["Erik"][:lottery_numbers].push(7)
 
 # 8. Change Erik's hometown to Edinburgh
 
-
+users["Erik"][:home_town] = "Edinburgh"
 
 # 9. Add a pet dog to Erik called "Fluffy"
 
-
+users["Erik"][:pets].push(name: "Fluffy", species: "Dog")
 
 # 10. Add another person to the users hash
 
+users.merge!("Malcolm" => {
+  :twitter => "Malc1970",
+  :lottery_numbers => [11, 12, 32, 8, 19, 45],
+  :home_town => "Stirling"})
 
 ## Exercise C
 
@@ -186,7 +190,25 @@ united_kingdom = [
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 
+united_kingdom[1][:capital] = "Cardiff"
 
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+
+united_kingdom.push({:name => "Northern Ireland",
+                     :population => 1811000,
+                     :capital => "Belfast"
+                     })
+
 # 3. Use a loop to print the names of all the countries in the UK.
+
+for country in united_kingdom
+  p country[:name]
+end
+
 # 4. Use a loop to find the total population of the UK.
+
+total = 0
+for country in united_kingdom
+  total += country[:population]
+end
+p total
